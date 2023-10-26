f36ddf9be019ec35b26bc7df2360eb618b819d5d
gurpreet singh <gurpreetsingh2591@gmail.com>
2023-10-26 21:48:30 +0530

    local changes

--- lib/widgets/ReminderBoxWidget.dart
+++ lib/widgets/ReminderBoxWidget.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kmschool/utils/extensions/lib_extensions.dart';
 import '../utils/constant.dart';
 
-
-
 class ReminderBoxWidget extends StatelessWidget {
   final String reminderName;
   final String id;
@@ -26,17 +24,11 @@ class ReminderBoxWidget extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         margin: const EdgeInsets.only(bottom: 15, right: 15),
         decoration: kInnerDecoration,
-        child: Column(
+        child: const Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.network(
-              image,
-              fit: BoxFit.contain,
-            ),
-
-          ],
+          children: [],
         ),
       ),
     );
